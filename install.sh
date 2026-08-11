556f4cbe263ab49015bee830ad0d6873325b1419
josv <99josv20@gmail.com>
2026-08-11 10:14:47 +0200

    Remove unecessary mkdir commands

--- install.sh
+++ install.sh
@@ -24,7 +24,8 @@ mkdir -p ~/.config/hypr && ln -sf ~/josv-linux/dotfiles/hypr/hyprland.lua ~/.con
 mkdir -p ~/.config/nvim && ln -sf ~/josv-linux/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
 mkdir -p ~/.config/uwsm && ln -sf ~/josv-linux/dotfiles/uwsm/env ~/.config/uwsm/env
 mkdir -p ~/.config/yazi && ln -sf ~/josv-linux/dotfiles/yazi/keymap.toml ~/.config/yazi/keymap.toml
-mkdir -p ~/.config/yazi && ln -sf ~/josv-linux/dotfiles/yazi/yazi.toml ~/.config/yazi/yazi.toml
+ln -sf ~/josv-linux/dotfiles/yazi/yazi.toml ~/.config/yazi/yazi.toml
+
 ln -sf ~/josv-linux/dotfiles/zsh/zprofile ~/.zprofile
 ln -sf ~/josv-linux/dotfiles/zsh/zshrc ~/.zshrc
 
