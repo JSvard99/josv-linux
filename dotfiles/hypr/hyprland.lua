e195ea70de5c1789d927ea17ed7a7657fbf8ea99
JoSv <99josv20@gmail.com>
2026-08-13 17:43:53 +0200

    Update Hyprland config

--- dotfiles/hypr/hyprland.lua
+++ dotfiles/hypr/hyprland.lua
@@ -14,7 +14,7 @@ hl.monitor({
 -------------------
 
 hl.on("hyprland.start", function () 
-	hl.exec_cmd("uwsm app -- noctalia")
+	hl.exec_cmd("uwsm-app -s s noctalia")
 end)
 
 -----------------------
@@ -23,8 +23,8 @@ end)
 
 hl.config({
     general = {
-	gaps_in  = 5,
-	gaps_out = 10,
+	gaps_in  = 3,
+	gaps_out = 6,
 
 	border_size = 1,
 
@@ -32,9 +32,6 @@ hl.config({
 	    active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
 	    inactive_border = "rgba(595959aa)",
 	},
-
-	-- Set to true to enable resizing windows by clicking and dragging on borders and gaps
-	resize_on_border = false,
 
 	-- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
 	allow_tearing = false,
