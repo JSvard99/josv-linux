254dcaae549e53cf5389798d137e4e8d42353fd3
JoSv <99josv20@gmail.com>
2026-08-17 16:46:25 +0200

    Refactor install script

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 
 
 # Install additional packages
-sudo pacman -S - < ~/josv-linux/pkglist.txt
+sudo pacman -S --noconfirm - < ~/josv-linux/pkglist.txt
 
 # Install yay
 git clone https://aur.archlinux.org/yay.git
@@ -20,24 +20,38 @@ yay -S catppuccin-cursors-mocha
 
 
 # Create symlinks
-mkdir -p ~/.config/ghostty && ln -sf ~/josv-linux/dotfiles/ghostty/config.ghostty ~/.config/ghostty/config.ghostty
-mkdir -p ~/.config/hypr && ln -sf ~/josv-linux/dotfiles/hypr/hyprland.lua ~/.config/hypr/hyprland.lua
-mkdir -p ~/.config/nvim && ln -sf ~/josv-linux/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
-mkdir -p ~/.config/uwsm && ln -sf ~/josv-linux/dotfiles/uwsm/env ~/.config/uwsm/env
-mkdir -p ~/.config/yazi && ln -sf ~/josv-linux/dotfiles/yazi/keymap.toml ~/.config/yazi/keymap.toml
+mkdir -p ~/.config/ghostty
+ln -sf ~/josv-linux/dotfiles/ghostty/config.ghostty ~/.config/ghostty/config.ghostty
+
+mkdir -p ~/.config/hypr
+ln -sf ~/josv-linux/dotfiles/hypr/hyprland.lua ~/.config/hypr/hyprland.lua
+
+mkdir -p ~/.config/nvim
+ln -sf ~/josv-linux/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
+
+mkdir -p ~/.config/uwsm
+ln -sf ~/josv-linux/dotfiles/uwsm/env ~/.config/uwsm/env
+
+mkdir -p ~/.config/yazi
+ln -sf ~/josv-linux/dotfiles/yazi/keymap.toml ~/.config/yazi/keymap.toml
 ln -sf ~/josv-linux/dotfiles/yazi/yazi.toml ~/.config/yazi/yazi.toml
 ln -sf ~/josv-linux/dotfiles/yazi/theme.toml ~/.config/yazi/theme.toml
 
 ln -sf ~/josv-linux/dotfiles/zsh/zprofile ~/.zprofile
 ln -sf ~/josv-linux/dotfiles/zsh/zshrc ~/.zshrc
 
-mkdir -p ~/.config/noctalia && ln -sf ~/josv-linux/dotfiles/noctalia/config.toml ~/.config/noctalia/config.toml
+mkdir -p ~/.config/noctalia
+ln -sf ~/josv-linux/dotfiles/noctalia/config.toml ~/.config/noctalia/config.toml
 
-mkdir -p ~/.config/fontconfig && ln -sf ~/josv-linux/dotfiles/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
+mkdir -p ~/.config/fontconfig
+ln -sf ~/josv-linux/dotfiles/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
 
-sudo mkdir -p /etc/keyd && sudo ln -sf ~/josv-linux/dotfiles/keyd/default.conf /etc/keyd/default.conf
+sudo mkdir -p /etc/keyd
+sudo ln -sf ~/josv-linux/dotfiles/keyd/default.conf /etc/keyd/default.conf
 
-mkdir -p ~/.config/lazygit && ln -sf ~/josv-linux/dotfiles/lazygit/config.yml ~/.config/lazygit/config.yml
+mkdir -p ~/.config/lazygit
+ln -sf ~/josv-linux/dotfiles/lazygit/config.yml ~/.config/lazygit/config.yml
+
 
 # Start/enable systemd services
 sudo systemctl start reflector.timer
